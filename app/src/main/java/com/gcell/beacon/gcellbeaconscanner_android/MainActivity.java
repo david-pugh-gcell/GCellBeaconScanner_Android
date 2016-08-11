a888905b490300067a77f5cc27dc0138d0e79dd3
david-pugh-gcell <david.pugh@g24i.com>
2016-08-11 14:15:50 +0100

    Version 1.01 of library used, with improved permission control, new GCelliBeacon class, improved JavaDocs

--- app/src/main/java/com/gcell/beacon/gcellbeaconscanner_android/MainActivity.java
+++ app/src/main/java/com/gcell/beacon/gcellbeaconscanner_android/MainActivity.java
@@ -5,15 +5,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+
+import com.gcell.ibeacon.gcellbeaconscanlibrary.GCellBeaconManagerScanEvents;
 import com.gcell.ibeacon.gcellbeaconscanlibrary.GCellBeaconRegion;
 import com.gcell.ibeacon.gcellbeaconscanlibrary.GCellBeaconScanManager;
-import com.gcell.ibeacon.gcellbeaconscanlibrary.GCellBleDevice;
+import com.gcell.ibeacon.gcellbeaconscanlibrary.GCelliBeacon;
 import com.gcell.ibeacon.gcellbeaconscanlibrary.GCellUuid;
 
 import java.util.ArrayList;
 import java.util.List;
 
- public class MainActivity extends Activity implements GCellBeaconScanManager.GCellBeaconManagerScanEvents{
+ public class MainActivity extends Activity implements GCellBeaconManagerScanEvents {
 
 	 private String TAG = "GCScanManager";
 	 private GCellBeaconScanManager mbtManager;
@@ -28,22 +30,35 @@ import java.util.List;
 		// Use the Beacon Scan manager to start scanning for beacons
 		// set up scan manager
 		mbtManager = new GCellBeaconScanManager(this);
+
+
+        /////////// You can configure some of the default settings
+        // Switch debug to true to get feedback from the library during development via Log.i
+        mbtManager.enableDeuggingFeedback(true);
+        // Set the library to automatically switch BLE on if it is off or switched off, default is true
+        mbtManager.enableBlueToothAutoSwitchOn(true);
+        // Set the auto-refresh rate to 10 s - this is the time we will go without seeing a beacon before deleting it from the ranged list. Default is 8s
+        mbtManager.setBeaconAutoRefreshRate(10);
+
+        //Set the message to display when showing information dialog before requesting permissions to access users location
+        mbtManager.setPermissionExplanationMessage("Example Feedback Message. We need permission sto see your location before we can see beacons!");
+        // Set the requestCode used to Request Grant Permissions - default is 1
+        mbtManager.setPermissionRequestCode(24);
+
+
+
 		//use known regions
 		mbtManager.useBeaconRegions(true);
 		//set known regions
-		GCellBeaconRegion gCellRegion = new GCellBeaconRegion(); //Sending no parameters initiates a region using the Default GCell UUID
+        //Sending no parameters initiates a region using the Default GCell UUID and label "com.gcell"
+		GCellBeaconRegion gCellRegion = new GCellBeaconRegion();
+
+        /*alternative is to use
+        GCellBeaconRegion exampleRegion = new GCellBeaconRegion( new GCellUuid((new GCellUuid("26530D1D-F9AF-4152-B99E-9B1A5E826584"), "com.example");
+         */
 		beaconRegions.add(gCellRegion);
 		mbtManager.setBeaconRegions(beaconRegions);
 		mbtManager.startMonitoringForBeacons();
-
-		/////////// You can also tweak other settings
-		// Switch debug to true to get feedback from the library during development
-		mbtManager.deBug = true;
-		// Set the library to automatically switch BLE on if it is off or switched off
-		mbtManager.autoSwitchOnBlueTooth = true;
-		// Set the auto-refresh rate
-		mbtManager.setBeaconAutoRefreshRate(10);
-
 	}
 
 
@@ -51,11 +66,11 @@ import java.util.List;
 	 // region Handle BeaconScanManager Events
 
 	 // This event means the scan manager has updated the ranged beacon list
-	 public void onGCellUpdateBeaconList(List<GCellBleDevice> disc_gcell_beacons) {
-		 Log.i(TAG, "Beacons found: " + disc_gcell_beacons.size());
+	 public void onGCellUpdateBeaconList(List<GCelliBeacon> disc_gcell_beacons) {
+		 Log.i(TAG, "No Beacons found: " + disc_gcell_beacons.size());
 	 }
 
-	 // This event means the device has enetred a beacon region. To find out more about what beacons are in the region, start ranging.
+	 // This event means the device has entered a beacon region. To find out more about what beacons are in the region, start ranging.
 	 // This will return beacon UUID, Major, Minor and RSSI values
 	 public void didEnterBeaconRegion(GCellBeaconRegion region) {
 		 Toast.makeText(this, "Entered region " + region.toString(),
@@ -73,7 +88,7 @@ import java.util.List;
 	 }
 
 	 // Beacons within a region have been ranged - we now have a list of beacons and their values
-	 public void didRangeBeaconsinRegion(GCellBeaconRegion region, List<GCellBleDevice> disc_gcell_beacons) {
+	 public void didRangeBeaconsinRegion(GCellBeaconRegion region, List<GCelliBeacon> disc_gcell_beacons) {
 		 Log.i(TAG, "Beacons found in region: " + disc_gcell_beacons.size() + " " + region.toString());
 
 	 }
@@ -97,7 +112,7 @@ import java.util.List;
 		 Toast.makeText(this, "BLE Not Enabled",
 				 Toast.LENGTH_SHORT).show();
 		 Log.i(TAG, "BLE not enabled, enabling...");
-         
+
 	 }
 	// endregion
 
@@ -111,7 +126,14 @@ import java.util.List;
 	  */
 	 @Override
 	 public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-		 mbtManager.permissionResult(requestCode, permissions, grantResults);
+         //Check to see if this is feedback from the request that the Scan manager made by checking the requestCode
+         if(requestCode == mbtManager.getPermissionRequestCode()) {
+             mbtManager.handlePermissionResult(requestCode, permissions, grantResults);
+         }else{
+             //This wasnt a response to the Scan Manager permission another request, handle accordingly
+             Log.i(TAG,"This was another request");
+         }
+
 	 }
 
  	// end region
